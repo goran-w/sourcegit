371639dac9370e9a59eafca3589e8a52a54e9633
Göran W <goran.wallgren@surgicalscience.com>
2025-06-04 09:34:07 +0200

    WIP

--- src/Views/Init.axaml.cs
+++ src/Views/Init.axaml.cs
@@ -1,4 +1,8 @@
+using System;
+
 using Avalonia.Controls;
+using Avalonia.Interactivity;
+using Avalonia.Platform.Storage;
 
 namespace SourceGit.Views
 {
@@ -7,6 +11,31 @@ namespace SourceGit.Views
         public Init()
         {
             InitializeComponent();
+        }
+
+        private async void SelectParentDir(object _, RoutedEventArgs e)
+        {
+            var toplevel = TopLevel.GetTopLevel(this);
+            if (toplevel == null)
+                return;
+
+            var options = new FolderPickerOpenOptions() { AllowMultiple = false };
+            try
+            {
+                var selected = await toplevel.StorageProvider.OpenFolderPickerAsync(options);
+                if (selected.Count == 1)
+                {
+                    var folder = selected[0];
+                    var folderPath = folder is { Path: { IsAbsoluteUri: true } path } ? path.LocalPath : folder?.Path.ToString();
+                    //ViewModels.Preferences.Instance.GitDefaultCloneDir = folderPath;
+                }
+            }
+            catch (Exception ex)
+            {
+                App.RaiseException(string.Empty, $"Failed to select parent directory: {ex.Message}");
+            }
+
+            e.Handled = true;
         }
     }
 }
