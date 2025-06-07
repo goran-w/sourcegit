3f77fd1604bfc76cfe7397e7a4ff125644c93ab3
Göran Wallgren <goran.wallgren@surgicalscience.com>
2025-06-08 00:22:04 +0200

    Add new main view/page for LOCAL FILES (in Working Copy)

---
+++ src/ViewModels/LocalFilesPage.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace SourceGit.ViewModels
+{
+    public class LocalFilesPage : ObservableObject, IDisposable
+    {
+        public LocalFilesPage(Repository repo)
+        {
+            _repo = repo;
+
+            var commitDetail = new CommitDetail(_repo);
+            var commit = new Commands.QuerySingleCommit(_repo.FullPath, "HEAD").Result();
+            commitDetail.Commit = commit;
+            DetailContext = commitDetail;
+        }
+
+        public IDisposable DetailContext
+        {
+            get => _detailContext;
+            set => SetProperty(ref _detailContext, value);
+        }
+
+        public void Dispose()
+        {
+            _repo = null;
+        }
+
+        private Repository _repo = null;
+        private IDisposable _detailContext = null;
+    }
+}
